46db97cd0bedc0aa439c8173c169de1638aadb3b
Rémi Faucheux <remifaucheux@MBP-de-Remi-2-001.lan>
2025-01-09 16:50:43 +0100

    nc

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -2,9 +2,14 @@
  * loads and decorates the hero
  * @param {Element} block The hero block element
  */
+import { readBlockConfig } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
   // Target the third child div to add button-primary class
+  const blockConfig = readBlockConfig(block);
+  console.log(blockConfig);
+  console.log(blockConfig.heading);
+  console.log(blockConfig['heading']);
   const div = block.children[2];
 
   if (div) {
