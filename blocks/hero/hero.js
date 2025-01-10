d51e1dd113e25e38567d370dd51e90b511faf350
Rémi Faucheux <remifaucheux@MBP-de-Remi-2-001.lan>
2025-01-10 09:25:16 +0100

    nc

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -16,20 +16,4 @@ export default async function decorate(block) {
     const btnContainer = div.querySelector('.button-container');
     btnContainer.querySelector('a').classList.add('button-primary');
   }
-
-
-    // Sélectionne tous les h4 dans la section héro
-    const headings = document.querySelectorAll('.hero-wrapper .hero.block > div:nth-child(2) h4');
-
-    // SVG à ajouter après chaque h4
-    const svgIcon = `
-    <svg class="c-header3Cta__linkIcon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-      <path d="M13.9,12,6.6,4.7,8,3.3,16.7,12,8,20.7,6.6,19.3Z" fill-rule="evenodd"></path>
-    </svg>
-  `;
-
-    // Parcours chaque h4 et ajoute l'élément SVG
-  headings.forEach(function(heading) {
-    heading.insertAdjacentHTML('afterend', svgIcon);
-  });
 }
